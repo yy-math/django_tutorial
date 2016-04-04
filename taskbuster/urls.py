10d49a5fda4e8bfbd47dd5c874916cd8978bc643
Hayley Mathews <yy.mathews@gmail.com>
2016-04-04 10:51:17 -0500

    template inheritance and testing with coverage

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import home
+from .views import home, home_files
 
 urlpatterns = [
     # Examples:
@@ -9,4 +9,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name ='home-files'),
 ]
